663f49048b454165c841970a4bda159611554ee4
Riot Cop <riot.cop.official@gmail.com>
2025-03-14 12:19:54 -0700

    javascript file add

---
+++ riotscript.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const heading = document.querySelector('h1');
+    heading.addEventListener('click', function() {
+       
+    });
+
+    const image = document.querySelector('img');
+    image.addEventListener('mouseover', function() {
+        image.style.border = '5px solid red';
+    });
+
+    image.addEventListener('mouseout', function() {
+        image.style.border = 'none';
+    });
+}); 
